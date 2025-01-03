ca797c5e0a776b201615236f0bd036b21160be0b
agrimshar <agrimsharma2004@gmail.com>
2025-01-03 02:41:00 -0500

    connected to port 9050 (tor) using sockets. needed to install tor and listen on port 9050 for the connection to work

--- toralize.c
+++ toralize.c
@@ -4,7 +4,8 @@
 int main(int argc, char *argv[]) 
 {
     char *host;
-    int port;
+    int port, s;
+    struct sockaddr_in sock;
 
     if (argc < 3)
     {
@@ -15,4 +16,28 @@ int main(int argc, char *argv[])
 
     host = argv[1];
     port = atoi(argv[2]);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0)
+    {
+        perror("socket");
+        return -1;
+    }
+
+    sock.sin_family = AF_INET;
+    sock.sin_port = htons(PROXYPORT);
+    sock.sin_addr.s_addr = inet_addr(PROXY);
+
+    if (connect(s, (struct sockaddr *)&sock, sizeof(sock)))
+    {
+    
+        perror("connect");
+        return -1;
+    }
+
+    printf("Connected to %s:%d\n", PROXY, PROXYPORT);
+    
+    close(s);
+
+    return 0;
 }
